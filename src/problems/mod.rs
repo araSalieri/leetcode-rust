0b3f9eb3774452c5ca9be0023daeddb5c386078a
araSalieri <rakhawiswara@gmail.com>
2026-04-09 00:22:08 +0700

    feat: add p0020 problem

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,3 +1,4 @@
 pub mod p0014_longest_common_prefix;
+pub mod p0020_valid_parentheses;
 pub mod p0026_remove_duplicates;
 pub mod p0027_remove_element;
---
+++ src/problems/p0020_valid_parentheses.rs
@@ -0,0 +1,86 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+// Example 5:
+
+// Input: s = "([)]"
+
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+pub fn is_valid(s: String) -> bool {
+    if s.len() == 1 {
+        return false;
+    }
+    let mut stack: Vec<char> = Vec::new();
+    let mut valid = false;
+    for c in s.chars() {
+        if c == '(' || c == '[' || c == '{' {
+            stack.push(c);
+            continue;
+        }
+
+        let close_bracket = stack.pop();
+        match close_bracket {
+            Some(v) => {
+                if (c == ')' && v == '(') || (c == ']' && v == '[') || (c == '}' && v == '{') {
+                    valid = true;
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            None => return false,
+        }
+    }
+
+    if let Some(_) = stack.pop() {
+        return false;
+    }
+
+    valid
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(is_valid(String::from("{}")), true);
+    }
+}
